f7720520f1831bf207ec8677722fca698873fee3
JulianDev <juliancamilogarciaescobar7@gmail.com>
2023-01-07 00:21:06 -0500

    final commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import IndexRouter from "./routes/IndexRouter";
+
+function App() {
+  return (
+    <div className="App">
+      <IndexRouter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React  from 'react'
+import "../styles/Home.css"
+import UploadPhoto from '../components/UploadPhoto'
+
+const Home = () => {
+
+    return (
+        <UploadPhoto />
+    )
+}
+
+export default Home
